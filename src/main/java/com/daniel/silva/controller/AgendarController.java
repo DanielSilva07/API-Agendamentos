98cb607590089e6d3a4900dfe736c744a47ef4ee
danielsilva07 <daniel97silva.ds@gmail.com>
2024-10-16 15:28:40 -0300

    ci

--- src/main/java/com/daniel/silva/controller/AgendarController.java
+++ src/main/java/com/daniel/silva/controller/AgendarController.java
@@ -37,7 +37,7 @@ public class AgendarController {
 
     @GetMapping("/teste")
     public String teste(){
-        return " MIAMI HEAT **  ";
+        return " AGNES ";
     }
 
     @GetMapping("/getAll")
