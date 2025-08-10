0917aeee75486f250ee5d930c2870e583bf1f0bd
DanielSilva07 <daniel97silva.ds@gmail.com>
2025-08-09 22:22:25 -0300

    adding Postgresql

--- src/main/java/com/daniel/silva/repository/AgendarRepository.java
+++ src/main/java/com/daniel/silva/repository/AgendarRepository.java
@@ -1,9 +1,14 @@
 package com.daniel.silva.repository;
 
 import com.daniel.silva.domain.model.AgendarModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AgendarRepository extends MongoRepository<AgendarModel,String> {
+public interface AgendarRepository extends JpaRepository<AgendarModel,Long> {
+
+     Optional<AgendarModel>findByNome(String nome);
+
 }
